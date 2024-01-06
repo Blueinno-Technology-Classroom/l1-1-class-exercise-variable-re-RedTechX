f2839d78400199aaa201935a6c0f6251d63be145
RedTechX <jeremy.fangyc@gmail.com>
2024-01-06 14:48:14 +0800

    hi

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 Q1: 
 '''
 
-x = None    # TODO: Replace `None` with your answer
+x = 10    # TODO: Replace `None` with your answer
 
 print('Q1:')
 print(f'x = {x}')
@@ -13,9 +13,9 @@ print('#' * 30)
 Q2:
 '''
 
-a = None    # TODO: Replace `None` with your answer
-b = None    # TODO: Replace `None` with your answer
-c = None    # TODO: Replace `None` with your answer
+a = 6    # TODO: Replace `None` with your answer
+b = 9    # TODO: Replace `None` with your answer
+c = 16   # TODO: Replace `None` with your answer
 
 print('Q2:')
 print(f'a = {a}')
@@ -27,8 +27,8 @@ print('#' * 30)
 Q3:
 '''
 
-first = None    # TODO: Replace `None` with your answer
-second = None   # TODO: Replace `None` with your answer
+first = 19    # TODO: Replace `None` with your answer
+second = 8   # TODO: Replace `None` with your answer
 
 print('Q3:')
 print(f'first = {first}')
@@ -39,7 +39,11 @@ print('#' * 30)
 Q4:
 '''
 
-# TODO: Write your code here
+first = 8
+second = 19
+first += second
+second = first - second
+first -= second
 
 ##################################################
 '''
@@ -48,6 +52,20 @@ Q5:
 
 # TODO: Write your code here
 
+work_hours = 4 + 5 + 8 + 4
+    
+# Calculate pay at work based on hours worked each day
+print("My total hours worked:")
+print(work_hours)
+
+print("My hourly salary:")
+print("$8.75")
+
+print("My total pay:")
+print(work_hours * 8.75)
+
+print("My taxes owed:")  # 20% tax
+print((work_hours) * 8.75 * 0.20)
 ##################################################
 '''
 Q6:
